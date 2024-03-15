96d580d80ecaed30961f33787d9428845c5747b4
Andy Botting <andy@andybotting.com>
2024-03-15 15:01:13 +1100

    Fix incorrect use of the catalog list API call
    The API actually returns the packages in a nested list, as documented
    at: https://docs.openstack.org/api-ref/application-catalog/v1/#packages
    
    Change-Id: Ib994d03782559c23f315e7fa1a6b2d26745aa96b

--- murano_tempest_tests/tests/scenario/application_catalog/test_deployment.py
+++ murano_tempest_tests/tests/scenario/application_catalog/test_deployment.py
@@ -52,7 +52,7 @@ class TestMuranoDeployment(base.BaseApplicationCatalogScenarioTest):
 
         # remove existing test packages
         existing_packages = cls.client.get_list_packages()
-        for package in existing_packages:
+        for package in existing_packages["packages"]:
             if package["fully_qualified_name"] in \
                     ['io.murano.test.apache.ApacheHttpServerCustom',
                      'io.murano.apps.test.Lighttpd',
